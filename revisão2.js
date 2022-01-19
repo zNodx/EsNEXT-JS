2ef361f0be6dc193d2e27a67c3ba263a1563228d
mateus <ffrmateus@gmail.com>
2022-01-19 13:26:58 -0300

    Arrow Function/Parâmetro default/Operador Rest - Revisão

---
+++ revisão2.js
@@ -0,0 +1,27 @@
+// Arrow Function
+const soma = (a,b) => a + b
+// const soma = (a,b) => {
+//     return a+b
+// }
+console.log(soma(2,3))
+
+const lexico1= () => console.log(this === exports)
+const lexico2 = lexico1.bind({})
+lexico1()
+lexico2()
+
+// parametro default
+
+function log(texto = 'Node'){
+    console.log(texto)
+}
+log()
+log("Sou mais forte")
+
+// Operador Rest
+function total(...numeros){
+    let total = 0
+    numeros.forEach(n => total += n)
+    return total
+}
+console.log(total(2,3,4,5))
